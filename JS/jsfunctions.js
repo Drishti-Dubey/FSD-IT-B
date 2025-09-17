3fbe2f7060807795b2c52a1069a6fe706c65369d
Drishti Dubey <drishtidubey4957@gmail.com>
2025-09-17 10:43:47 +0530

    17/9/25

--- JS/jsfunctions.js
+++ JS/jsfunctions.js
@@ -1,32 +1,225 @@
-function hello(user='Guest')
-{
-    console.log(`Hello ${user}`);
+// function hello(user='Guest')
+// {
+//     console.log(`Hello ${user}`);
+// }
+
+// hello('Drishti');
+// hello();
+
+// function sum(a,b)
+// {
+//     console.log('Before written');
+//     return(a+b);
+// }
+// console.log(sum(23,24));
+
+// const hello=()=>console.log('hello')
+// hello()
+
+// function hello(user,callback)
+// {
+//     console.log("hello");
+//     callback();
+// }
+
+// hello('Admin',function()
+// {
+//     console.log(add(5,5))
+// });
+// hello('Admin',()=>
+// {
+//     console.log(add(6,7))
+// });
+
+
+function register(){
+    console.log('register end');
+}
+function sendEmail(){
+    console.log('send email end');
+}
+function login(){
+    console.log('login end');
+}
+function getData(){
+    console.log('get data end');
+}
+function displayData(){
+    console.log('display Data End')
 }
 
-hello('Drishti');
-hello();
+register();
+sendEmail();
+login();
+getData();
+displayData();
+console.log('call other application');
 
-function sum(a,b)
-{
-    console.log('Before written');
-    return(a+b);
+
+
+
+function register(){
+    waitforfiveseconds();
+    console.log('register end');
 }
-console.log(sum(23,24));
-
-const hello=()=>console.log('hello')
-hello()
-
-function hello(user,callback)
-{
-    console.log("hello");
-    callback();
+function sendEmail(){
+    waitforfiveseconds();
+    console.log('send email end');
+}
+function login(){
+    waitforfiveseconds();
+    console.log('login end');
+}
+function getData(){
+    waitforfiveseconds();
+    console.log('get data end');
+}
+function displayData(){
+    waitforfiveseconds();
+    console.log('display Data End')
+}
+function waitforfiveseconds(){
+    const ms=new Date().getTime()+5000;
+    while(new Date().getTime()<ms)
+    {}
 }
 
-hello('Admin',function()
-{
-    console.log(add(5,5))
+register();
+sendEmail();
+login();
+getData();
+displayData();
+console.log('call other application');
+
+
+
+
+function register(cb){
+    setTimeout(()=>{
+       console.log('register end');
+       cb();
+    },5000)
+    
+}
+function sendEmail(cb){
+     setTimeout(()=>{
+       console.log('send email end');
+       cb();
+    },3000)
+    
+}
+function login(cb){
+    setTimeout(()=>{
+       console.log('login end');
+       cb();
+    },1000)
+    
+}
+function getData(cb){
+    setTimeout(()=>{
+        console.log('get data end');
+        cb();
+    },2000)
+   
+}
+function displayData(){
+     setTimeout(()=>{
+        console.log('display Data End')
+    },4000)
+    
+}
+//callback Hell
+register(()=>{
+           sendEmail(()=>{
+                      login(()=>{
+                            getData(()=>{
+                                displayData();
+                            });
+                      });
+           });
 });
-hello('Admin',()=>
-{
-    console.log(add(6,7))
-});
+
+console.log('call other application');
+
+
+function register(){
+    return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+       console.log('register end');
+       resolve();
+    },5000)
+    })
+    
+    
+}
+function sendEmail(){
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+       console.log('send email end');
+       resolve();
+    },3000)
+    })
+     
+    
+}
+function login(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+       console.log('login end');
+       resolve();
+    },1000)
+    })
+    
+    
+}
+function getData(){
+    return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+        console.log('get data end');
+        resolve();
+    },2000)
+    })
+    
+   
+}
+function displayData(){
+    return new Promise((resolve,reject)=>{
+          setTimeout(()=>{
+        console.log('display Data End');
+        resolve();
+    },4000)
+    })
+     
+    
+}
+//callback Hell
+// register(()=>{
+//            sendEmail(()=>{
+//                       login(()=>{
+//                             getData(()=>{
+//                                 displayData();
+//                             });
+//                       });
+//            });
+// });
+//   register()
+//   .then(sendEmail)
+//   .then(login)
+//   .then(getData)
+//   .then(displayData)
+//   .catch((err)=>{console.log('Error: '+err)})
+//   .finally(()=>{console.log('inside finally')})
+async function f1(){
+    try{
+await register();
+await sendEmail();
+await login();
+await getData();
+await displayData();
+    }
+    catch(err){
+        console.log('Error :'+err);
+    }
+}
+f1();
+console.log('call other application');
