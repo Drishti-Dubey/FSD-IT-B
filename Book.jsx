41dee0590b74518960c8f72c59731af5fd07c496
Drishti-Dubey <drishtidubey4957@gmail.com>
2025-11-03 13:05:00 +0530

    3/11/2025

--- Book.jsx
+++ Book.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-function Book({book}){
-    return (
-        <div className='card'>
-            <img src={book.image} width="200" height="200" alt="image"/>
-            <h3>Title:{book.title}</h3>
-            <h4>{book.price}</h4>
-            <button>Add to Cart</button>
-        </div>
-    )
+import React from 'react'
+import './Book.css'
+const Book = () => {
+  return (
+    <div className='card'>
+      <img src="" width='200' height='200' alt="image"/>
+      <h3>Title:Phyics Book</h3>
+      <h4>Price:₹462/-</h4>
+      <button>Add to Cart!</button>
+    </div>
+  )
 }
-export default Book;
+
+export default Book
