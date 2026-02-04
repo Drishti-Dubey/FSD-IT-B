1902ded8baebc657f57f7c1d81bcf9300f156062
Drishti-Dubey <drishtidubey4957@gmail.com>
2026-02-04 09:41:06 +0530

    04/02/2026

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
-function Book(book)
-{
+// server creation
+const http=require('http');
 
-const image = React.createElement("img", { 
-  src: book.image, 
-  width: 200, 
-  height: 200, 
-  alt: "Book Image" 
-}, null);
+ const myserver=http.createServer((req,res)=>{
+  console.log('server1');
+  res.end('Hi! This is my first server!'); 
+});
 
-const h3 = React.createElement("h3", null,book.title);
-const h4 = React.createElement("h4", null,book.price);
-const btn = React.createElement("button", null, "ADD TO CART");
-
-const div = React.createElement("div", { className: "card" }, image, h3, h4, btn);
-return div;
-}
-
-const books=[
-    {    image:" ",
-    tile:Physics,
-    price:451},
-
-    {image:" ",
-    tile:Physsics,
-    price:452},
-
-    { image:" ",
-    tile:Physssics,
-    price:453}
-]
-
-const bookele=books.map((b,i)=>(
-    React.createElement(Book,{
-        key:i,
-        image:b.image,title:b.title,price:b.price
-    },null)
-))
-
-const booklist=React.createElement("div",{className:"booklist"},bookele);
-const parent = document.getElementById("root");
-const root=ReactDOM.createRoot(parent);
-root.render(booklist);
-
+myserver.listen(8000,()=>{
+  console.log('Server is running on port 8000');
+});
